49c701b37248feeacdeadddd99102d4f05f4ee5b
Nilskruthoff <nils.kruthoff@iubh-fernstudium.de>
2021-08-21 14:52:07 +0200

    Initial commit

---
+++ datetime.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+    renderLocalStoredDateTime('datetime', getLocalStoredDateTime('dateTime'));
+}
+
+function saveDate() {
+    let now = new Date();
+    let currentDate = now.getDate() + "." + (now.getMonth() + 1) + "." + now.getFullYear();
+    let currentTime = now.getHours() + ":" + minuteCheck(now.getMinutes());
+    let dateTime = currentDate + " " + currentTime;
+    document.getElementById("datetime").innerHTML = dateTime;
+    setLocalStoredDateTime("dateTime", dateTime);
+}
+
+function minuteCheck(minute) {
+    if (minute < 10) {
+        minute = "0" + minute;
+    }
+    return minute;
+}
+
+function setLocalStoredDateTime(keyName, dateTime) {
+    localStorage.setItem(keyName,dateTime);
+}
+
+function getLocalStoredDateTime(keyName) {
+    return localStorage.getItem(keyName);
+}
+
+function renderLocalStoredDateTime(id, dateTime) {
+    document.getElementById(id).innerHTML = dateTime;
+}
---
+++ search.js
@@ -0,0 +1,10 @@
+import "mark.js"
+
+
+function search() {
+    let userInput = document.getElementById('searchText').value;
+    let context = document.querySelectorAll("body");
+    let instance = new Mark(context);
+    instance.unmark(userInput);
+    instance.mark(userInput);
+}
